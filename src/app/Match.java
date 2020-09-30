4f013d3bd887cb092649eeb48d27faae0d2e5dd8
Naessens Bastien <bastien.naessens.etu@univ-lille.fr>
2020-09-30 11:19:38 +0200

    ajout des premiers tests

--- src/app/Match.java
+++ src/app/Match.java
@@ -1,6 +1,21 @@
 package app;
 
 public class Match {
+	private Competitor competitor1;
+	private Competitor competitor2;
+	
+	public Match( Competitor c1, Competitor c2) {
+		competitor1 = c1;
+		competitor2 =c2;
+	}
+	
+	public Competitor getCompetitor1() {
+		return competitor1;
+	}
+	public Competitor getCompetitor2() {
+		return competitor2;
+	}
+	
 	
 	
 }
