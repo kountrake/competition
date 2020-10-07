71a199089d3e7e564eab82c2e68f53ec2a65235e
Naessens Bastien <bastien.naessens.etu@univ-lille.fr>
2020-10-07 23:23:27 +0200

    Ajout des tests de la classe LeagueTest

--- TP1/test/app/LeagueTest.java
+++ TP1/test/app/LeagueTest.java
@@ -2,6 +2,14 @@ package app;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static util.MathUtils.fact;
+import static util.MathUtils.sumArray;
 
 class LeagueTest {
 
@@ -9,13 +17,32 @@ class LeagueTest {
     Competitor c2;
     Competitor c3;
     Competitor c4;
+    League competition;
+
 
     @BeforeEach
-    void initialize(){
+    void setUp() {
         c1 = new Competitor("Jhon");
         c2 = new Competitor("Doe",5);
         c3 = new Competitor("Timo",2);
         c4 = new Competitor("Leon",7);
+        ArrayList<Competitor> competitors = new ArrayList<>();
+        competitors.add(c1);
+        competitors.add(c2);
+        competitors.add(c3);
+        competitors.add(c4);
+        competition = new League(competitors);
+        competition.play();
+    }
+
+    @Test
+    void mostWinTest() {
+        assertEquals(Collections.max(competition.getWins()), competition.getWins().get(competition.mostWin()));
+    }
+
+    @Test
+    void playTest(){
+        assertEquals(fact((competition.getCompetitors().size()-1))*2,sumArray(competition.getWins()));
     }
 
 }
