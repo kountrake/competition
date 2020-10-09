4beff532bc85f2c2dd1a71e1e2fb8f609a20f674
Naessens Bastien <bastien.naessens.etu@univ-lille.fr>
2020-10-09 14:50:45 +0200

    Avancer dans les test de tournamentTest ainsi que dans la classe Tournament

--- TP1/src/app/Tournament.java
+++ TP1/src/app/Tournament.java
@@ -1,40 +1,96 @@
 package app;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The type Tournament.
+ */
 public class Tournament extends Competition {
 
-    private List<Competitor> competitors;
-    ArrayList<Competitor> competitorsRemaining;
+    private final ArrayList<Competitor> competitorsRemaining;
 
+    /**
+     * Instantiates a new Tournament.
+     *
+     * @param competitors the competitors
+     */
     public Tournament(List<Competitor> competitors) {
         super(competitors);
         competitorsRemaining = new ArrayList<>();
     }
 
-    private void oddCompetitors(List<Competitor> c, ArrayList<Competitor> competitorsRemaining) {
+    /**
+     * Gets competitors remaining.
+     *
+     * @return the competitors remaining
+     */
+    public ArrayList<Competitor> getCompetitorsRemaining() {
+        return competitorsRemaining;
+    }
+
+    /**
+     * Odd competitors.
+     *
+     * @param c                    the c
+     * @param competitorsRemaining the competitors remaining
+     */
+    public void oddCompetitors(List<Competitor> c, ArrayList<Competitor> competitorsRemaining) {
         if (c.size() % 2 != 0) {
             competitorsRemaining.add(c.get(0));
             c.remove(0);
         }
     }
 
-    private void playARound(List<Competitor> c, ArrayList<Competitor> competitorsRemaining) {
+    /**
+     * Play a round.
+     *
+     * @param c                    the c
+     * @param competitorsRemaining the competitors remaining
+     */
+    public void playARound(List<Competitor> c, ArrayList<Competitor> competitorsRemaining) {
         for (int i = 1; i < c.size(); i += 2) {
-            match.setCompetitor1(c.get(i - 1));
-            match.setCompetitor1(c.get(i));
-            match.play();
-            if (match.getCompetitor1().getWins() == 1) {
-                competitorsRemaining.add(c.get(i - 1));
-                match.getCompetitor1().increaseWins();
-            } else {
-                competitorsRemaining.add(c.get(i));
-                match.getCompetitor2().increaseWins();
-            }
+            playAMatch(c, i);
+            winnerOfTheMatch(c, competitorsRemaining, i);
         }
     }
 
+    /**
+     * Winner of the match.
+     *
+     * @param c                    the c
+     * @param competitorsRemaining the competitors remaining
+     * @param i                    the
+     */
+    public void winnerOfTheMatch(List<Competitor> c, ArrayList<Competitor> competitorsRemaining, int i) {
+        if (match.getCompetitor1().getWins() == 1) {
+            competitorsRemaining.add(c.get(i - 1));
+            match.getCompetitor1().increaseWins();
+        } else {
+            competitorsRemaining.add(c.get(i));
+            match.getCompetitor2().increaseWins();
+        }
+    }
+
+    /**
+     * Play a match.
+     *
+     * @param c the c
+     * @param i the
+     */
+    public void playAMatch(List<Competitor> c, int i) {
+        match.setCompetitor1(c.get(i - 1));
+        match.setCompetitor1(c.get(i));
+        match.play();
+    }
+
+    /**
+     * Process competitor.
+     *
+     * @param c the c
+     * @return the competitor
+     */
     public Competitor process(List<Competitor> c) {
         if (c.size() == 1) {
             return c.get(0);
@@ -44,6 +100,7 @@ public class Tournament extends Competition {
         return process(competitorsRemaining);
     }
 
+    @Override
     public void play() {
         process(competitors);
     }
--- TP1/test/app/TournamentTest.java
+++ TP1/test/app/TournamentTest.java
@@ -1,7 +1,43 @@
 package app;
 
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class TournamentTest {
 
-    
+    Competitor c1;
+    Competitor c2;
+    Competitor c3;
+    Competitor c4;
+    Competitor c5;
+    Tournament competition;
+
+
+    @BeforeEach
+    void setUp() {
+        c1 = new Competitor("Jhon");
+        c2 = new Competitor("Doe", 5);
+        c3 = new Competitor("Timo", 2);
+        c4 = new Competitor("Leon", 7);
+        c5 = new Competitor("Test", 3);
+        ArrayList<Competitor> competitors = new ArrayList<>();
+        competitors.add(c1);
+        competitors.add(c2);
+        competitors.add(c3);
+        competitors.add(c4);
+        competitors.add(c5);
+        competition = new Tournament(competitors);
+    }
+
+    @Test
+    void playTest() {
+        assertEquals(5, competition.competitors.size());
+        competition.play();
+        assertEquals(1, competition.competitors.size(), "Problem with the amount of winner");
+    }
 }
